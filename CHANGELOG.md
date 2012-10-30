8b1d748619eec5d3811e3ee0ea355fb17e720eae
JP Richardson <jprichardson@gmail.com>
2012-10-30 15:50:35 -0500

    Added lastModified option.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+0.0.2 / 2012-10-30
+------------------
+* Added `lastModified` and `options` field.
+
 0.0.1 / 2012-10-29
 ------------------
 * Initial release.
--- test/markdown-walker.test.js
+++ test/markdown-walker.test.js
@@ -6,7 +6,12 @@ var mkdw = require('../lib/markdown-walker')
 var TEST_DIR = '';
 
 describe('markdown-walker', function () {
+  var date = null;
+
   beforeEach(function(done) {
+    if (TEST_DIR)
+      fs.removeSync(TEST_DIR)
+
     TEST_DIR = testutil.createTestDir('test-markdown-walker')
     var files = [path.join(TEST_DIR, 'hi.md')
       , path.join(TEST_DIR, 'test.markdown')
@@ -21,6 +26,7 @@ describe('markdown-walker', function () {
       fs.writeFileSync(file, '')
     })
 
+    date = new Date().getTime();
     done()
   })
 
@@ -39,5 +45,30 @@ describe('markdown-walker', function () {
       })
 
     })
+
+    describe('> when lastModified', function() {
+      it('should walk the directory and emit markdown events if the last modified time is greater than or equal', function(done) {
+        
+        setTimeout(function(){
+          fs.writeFileSync(path.join(TEST_DIR, 'mod.md'), '')
+
+          var walker = mkdw(TEST_DIR, {lastModified: date})
+            , files = [];
+
+          walker.on('markdown', function(markdown) {
+            files.push(markdown);
+          })
+
+          walker.on('end', function() {
+            T (files.length === 1)
+            T (path.basename(files[0]) === 'mod.md')
+            done()
+          })
+        },1000); //have to at least use 1 sec because file stat times are in chopped to secs.
+        
+      })
+    })
   })
+
+  
 })
